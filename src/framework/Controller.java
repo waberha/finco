2140e8bba7a9b9c98fbb99bf8336feef57669579
Wendessen <waberha@miu.edu>
2022-02-01 22:47:36 -0600

    minor updates

--- src/framework/Controller.java
+++ src/framework/Controller.java
@@ -1,6 +1,7 @@
 package framework;
 
 import framework.account.Account;
+import framework.command.Command;
 import framework.command.CommandManager;
 import framework.command.impl.*;
 import framework.party.Party;
@@ -19,37 +20,41 @@ public class Controller {
 
     public Account createParty(Party party, Account account) {
         CreatePartyCommand command = commandFactory.createCreatePartyCommand(party, account);
-        commandManager.execute(command);
+        commandManager.submit(command);
         return command.getAccount();
     }
 
     public void removeParty(String name) {
         RemovePartyCommand command = commandFactory.createRemovePartyCommand(name);
-        commandManager.execute(command);
+        commandManager.submit(command);
     }
 
     public Account createAccount(String name, Account account) {
         CreateAccountCommand command = commandFactory.createCreateAccountCommand(name, account);
-        commandManager.execute(command);
+        commandManager.submit(command);
         return command.getAccount();
     }
 
     public void removeAccount(String accountNo) {
         RemoveAccountCommand command = commandFactory.createRemoveAccountCommand(accountNo);
-        commandManager.execute(command);
+        commandManager.submit(command);
     }
 
     public void withdrawMoney(String accountNo, double amount) {
         WithdrawMoneyCommand command = commandFactory.createWithdrawMoneyCommand(accountNo, amount);
-        commandManager.execute(command);
+        commandManager.submit(command);
     }
 
     public void depositMoney(String accountNo, double amount) {
         DepositMoneyCommand command = commandFactory.createDepositMoneyCommand(accountNo, amount);
-        commandManager.execute(command);
+        commandManager.submit(command);
     }
 
     public String generateReport(ReportingStrategy reportingStrategy) {
         return reportingStrategy.generateReport();
+    }
+
+    public void runCommand(Command<?> command) {
+        commandManager.submit(command);
     }
 }
